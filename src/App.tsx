be70fc9c14f859adc9548a94b4fc977889d8c27d
Joel Watkins <eck087@yahoo.co.uk>
2023-04-09 20:53:35 +0100

    initial setup, global styles

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="test">
+      <h1>Mugular</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam, omnis.
+        Maiores molestiae reiciendis iste sunt itaque aperiam laboriosam. Qui
+        nemo esse nisi inventore, quos eaque velit ipsam aspernatur, tempora
+        labore officiis animi eveniet deserunt rem. Earum beatae fugiat illo
+        temporibus velit exercitationem optio dignissimos ab, eum sequi unde
+        repudiandae corporis.
+      </p>
+    </div>
+  );
+}
+
+export default App;
