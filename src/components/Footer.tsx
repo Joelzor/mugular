3799f71755d201ad318ca71dbc3402f06d5c2f78
Joel Watkins <eck087@yahoo.co.uk>
2023-04-10 12:28:36 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import styled from "styled-components";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Wrapper>
+      <h5>
+        &copy; {new Date().getFullYear()}
+        <span> Mugular</span>
+      </h5>
+      <h5>All rights reserved</h5>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.footer`
+  height: 5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  justify-content: center;
+  align-items: center;
+  background: var(--grey-800);
+  text-align: center;
+
+  span {
+    color: var(--primary-500);
+  }
+
+  h5 {
+    color: var(--white);
+    margin: 0.1rem;
+
+    font-weight: 400;
+    text-transform: none;
+    line-height: 1.25;
+  }
+
+  @media (min-width: 776px) {
+    flex-direction: row;
+  }
+`;
 
 export default Footer;
