90a06be29f19c11aa886f4f3032a5da115d4d54e
Joel Watkins <eck087@yahoo.co.uk>
2023-04-11 16:12:34 +0100

    fetching all products from airbase function

--- functions/products.js
+++ functions/products.js
@@ -1,6 +1,52 @@
+require("dotenv").config();
+const Airtable = require("airtable-node");
+
+const airtable = new Airtable({
+  apiKey: process.env.VITE_AIRTABLE_KEY,
+})
+  .base(process.env.VITE_AIRTABLE_BASE_ID)
+  .table(process.env.VITE_AIRTABLE_TABLE);
+
 exports.handler = async (event, context, cb) => {
-  return {
-    statusCode: 200,
-    body: "products route",
-  };
+  try {
+    const response = await airtable.list({ maxRecords: 200 });
+
+    const products = response.records.map((product) => {
+      const { id, fields } = product;
+      const {
+        name,
+        featured,
+        price,
+        colors,
+        description,
+        category,
+        shipping,
+        images,
+      } = fields;
+
+      const { url } = images[0];
+      return {
+        id,
+        name,
+        featured,
+        price,
+        colors,
+        description,
+        category,
+        shipping,
+        image: url,
+      };
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: "there was an error",
+    };
+  }
 };
