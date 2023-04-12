e1d058536b21afb23b9f7c7df83f0d170628cbc4
Joel Watkins <eck087@yahoo.co.uk>
2023-04-12 17:00:20 +0100

    single product details displayed

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -3,11 +3,15 @@ import styled from "styled-components";
 import { useAppSelector, useAppDispatch } from "../hooks";
 import { useParams } from "react-router-dom";
 import { getSingleProduct } from "../features/products/singleProductSlice";
+import { Link } from "react-router-dom";
+import { formatPrice } from "../utils/helpers";
 
 const SingleProduct = () => {
   const { id } = useParams();
   const dispatch = useAppDispatch();
-  const { singleProduct } = useAppSelector((store) => store.singleProduct);
+  const { singleProduct: product } = useAppSelector(
+    (store) => store.singleProduct
+  );
 
   useEffect(() => {
     if (id) {
@@ -15,7 +19,80 @@ const SingleProduct = () => {
     }
   }, [id]);
 
-  return <div>SingleProduct</div>;
+  const {
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviews,
+    id: sku,
+    images,
+  } = product;
+
+  console.log(product);
+
+  return (
+    <Wrapper>
+      <div className="section section-center page">
+        <Link to="/products" className="btn">
+          back to products
+        </Link>
+        <div className="product-center">
+          <div></div>
+          <section className="content">
+            <h2>{name} Mug</h2>
+            <h5 className="price">{formatPrice(price)}</h5>
+            <p className="desc">{description}</p>
+            <p className="info">
+              <span>Available:</span>
+              {stock > 0 ? "In stock" : "Out of stock"}
+            </p>
+            <p className="info">
+              <span>SKU:</span>
+              {sku}
+            </p>
+            <hr />
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.main`
+  .product-center {
+    display: grid;
+    gap: 4rem;
+    margin-top: 2rem;
+  }
+  .price {
+    color: var(--primary-500);
+    font-weight: 600;
+  }
+  .desc {
+    line-height: 2;
+    max-width: 45em;
+  }
+  .info {
+    text-transform: capitalize;
+    width: 300px;
+    display: grid;
+    grid-template-columns: 125px 1fr;
+    span {
+      font-weight: 700;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .product-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+    }
+    .price {
+      font-size: 1.25rem;
+    }
+  }
+`;
 
 export default SingleProduct;
--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,6 +1,6 @@
 export const formatPrice = (number: number) => {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "USD",
+    currency: "GBP",
   }).format(number / 100);
 };
