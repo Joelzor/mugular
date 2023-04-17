4e64c9f9c8fee8f59f62ea7a064d9cb298e871f2
Joel Watkins <eck087@yahoo.co.uk>
2023-04-17 16:40:43 +0100

    Cart page initial setup

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -14,6 +14,7 @@ import Amounts from "./Amounts";
 import Filters from "./Filters";
 import Sort from "./Sort";
 import ProductList from "./ProductList";
+import CartContent from "./CartContent";
 
 export {
   Navbar,
@@ -32,4 +33,5 @@ export {
   Filters,
   Sort,
   ProductList,
+  CartContent,
 };
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,5 +1,42 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useAppSelector } from "../hooks";
+import { CartContent } from "../components";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cart } = useAppSelector((store) => store.cart);
+
+  if (cart.length < 1) {
+    return (
+      <Wrapper className="page-100">
+        <div className="empty">
+          <h2>Your cart is empty</h2>
+          <Link to="/products" className="btn">
+            shop
+          </Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <main>
+      <Wrapper className="page">
+        <CartContent />
+      </Wrapper>
+    </main>
+  );
 };
+
+const Wrapper = styled.main`
+  .empty {
+    text-align: center;
+
+    h2 {
+      margin-bottom: 2rem;
+      text-transform: none;
+    }
+  }
+`;
 
 export default Cart;
